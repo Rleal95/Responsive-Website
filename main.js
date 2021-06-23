41f9f81d183c87d10197ab771737e91b705ed42c
Rleal95 <rsldesign19@gmail.com>
2021-06-23 13:19:51 -0300

    Finalizado

--- main.js
+++ main.js
@@ -3,25 +3,25 @@ const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
 for (const element of toggle) {
-  element.addEventListener('click', function() {
+  element.addEventListener('click', function () {
     nav.classList.toggle('show')
   })
 }
 /*quando clicar em um item do menu,esconder o menu*/
-const links = document.querySelectorAll('nav ul li a') 
+const links = document.querySelectorAll('nav ul li a')
 
-for(const link of links){
- link.addEventListener('click', function() {
-  nav.classList.remove('show')
- })
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
 }
 
 /*mudar o header da página quando der scroll*/
-const header = document.querySelector ('#header')
+const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function(){
-  if(window.scrollY >= navHeight){
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
     //scroll é maior que a altura do header
     header.classList.add('scroll')
   } else {
@@ -29,3 +29,30 @@ window.addEventListener('scroll', function(){
     header.classList.remove('scroll')
   }
 })
+
+/*TESTIMONIALS CAROUSEL SLIDER WIPER*/
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+/*SCROLL REVEAL JS: Mostrar elementos quando der scroll na página*/
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home, text, 
+  #about .image, #about .text,
+  #services header, #services .card
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+)
